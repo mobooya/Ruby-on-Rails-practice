3305ea5fa7c93d0f18dbd6def4a7d43c5b92b450
Mohamed Ahmad <mobooya@gmail.com>
2012-05-20 17:18:03 -0700

    Finish demo app

---
+++ motwitter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motwitter::Application.config.secret_token = '54d67969778864e44a41164c86c5dba20067f93034616162d798815a3e6843bc63a86f5a350c0b44ecba09b2735b40b0c20152295bdd67e2e6b3f6925fa12973'
